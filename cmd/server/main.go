f09566d67099a3fe179ff28582021549ece7a192
Ben Eirich <beirich@gmail.com>
2024-09-21 12:16:24 -0400

    3.1 Publishing to an exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -13,15 +17,23 @@ func main() {
 	connectionString := "amqp://guest:guest@localhost:5672/"
 	con, err := amqp.Dial(connectionString)
 	if err != nil {
-		fmt.Printf("Error connecting to RabbitMQ. Is it running?\n %v\n", err)
-		os.Exit(-1)
+		log.Fatalf("Error connecting to RabbitMQ. Is it running?\n %v\n", err)
 	}
 	defer con.Close()
 	fmt.Println("RabbitMQ connection successful!")
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+
+	mqch, err := con.Channel()
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	err = pubsub.PublishJSON(mqch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Failed to publish: %v \n", err)
+	}
 
 	fmt.Println("Shutting down.")
 }
