c87861a8c223b1aa6a5c863dc548328027b3c2ca
Ben Eirich <beirich@gmail.com>
2024-09-21 15:34:27 -0400

    3.6 Decoupling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 
@@ -22,20 +21,36 @@ func main() {
 	defer con.Close()
 	fmt.Println("RabbitMQ connection successful!")
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-
 	mqch, err := con.Channel()
 	if err != nil {
 		log.Fatalf("%v\n", err)
 	}
 
-	err = pubsub.PublishJSON(mqch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
-	if err != nil {
-		log.Fatalf("Failed to publish: %v \n", err)
-	}
+	gamelogic.PrintServerHelp()
 
-	<-signalChan
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		} else if input[0] == "quit" {
+			fmt.Println("Exiting")
+			break
+		} else if input[0] == "help" {
+			gamelogic.PrintServerHelp()
+		} else if input[0] == "pause" {
+			err = pubsub.PublishJSON(mqch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Fatalf("Failed to publish: %v \n", err)
+			}
+		} else if input[0] == "resume" {
+			err = pubsub.PublishJSON(mqch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Fatalf("Failed to publish: %v \n", err)
+			}
+		} else {
+			fmt.Printf("I didn't understand '%s' command.\n", input[0])
+		}
+	}
 
 	fmt.Println("Shutting down.")
 }
